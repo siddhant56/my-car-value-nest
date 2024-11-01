ee48fc484bdcb2f6c259c7bf24bc9421f95fa8ab
siddhant56 <siddhanttrivedi56@gmail.com>
2024-11-02 00:42:41 +0530

    Add User

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller,Post } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user-dto';
+import { UsersService } from './users.service';
 
-@Controller('users')
-export class UsersController {}
+@Controller('auth')
+export class UsersController {
+
+    constructor(private userService:UsersService){
+
+    }
+    @Post('/signup')
+    createUser(@Body() user:CreateUserDto){
+        this.userService.create(user.email,user.password)
+    }
+}
